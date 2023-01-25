93a63f116b4633bbe68e531d2059260b9f1527c7
TatianaHoza <tatiana-15@tut.by>
2023-01-25 10:25:51 +0300

    add exc.cs

---
+++ exs.cs
@@ -0,0 +1,12 @@
+﻿string [] array = {"hello","2","world",":-)"};
+string [] new_array = new string[5];
+int temp = 0;
+for (int i = 0 ; i < array.Length;i++)
+{   
+    if (array[i].Length <=3)
+    new_array[temp] = array[i];
+    Console.Write($"{new_array[temp]}");
+    temp++;
+}
+
+
